5e5dc54210a81394ecc876c54b0d89b7661f086c
eazy03 <eazyratu06@gmail.com>
2025-10-06 13:46:06 +0700

    final code

---
+++ lib/data/post_data.dart
@@ -0,0 +1,24 @@
+import '../models/post.dart';
+
+final List<Post> dummyPosts = [
+  Post(
+    user: "john_doe",
+    caption: "Enjoying the sunset 🌅",
+    imageUrl: "https://picsum.photos/400/300?random=1",
+  ),
+  Post(
+    user: "alice99",
+    caption: "Coffee time ☕",
+    imageUrl: "https://picsum.photos/400/300?random=2",
+  ),
+  Post(
+    user: "michael",
+    caption: "Weekend vibes 🎶",
+    imageUrl: "https://picsum.photos/400/300?random=3",
+  ),
+  Post(
+    user: "gemparcr",
+    caption: "Coding all day 💻",
+    imageUrl: "https://picsum.photos/400/300?random=4",
+  ),
+];
